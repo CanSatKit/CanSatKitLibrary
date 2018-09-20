36e403de1ba90ef45ab1de1ef5a884b7c27b693e
begreeen <michal.gumiela@gmail.com>
2018-09-20 22:33:48 +0200

    more comments

--- examples/ErrorHandling/ErrorHandling.ino
+++ examples/ErrorHandling/ErrorHandling.ino
@@ -3,26 +3,38 @@
 void setup() {
   SerialUSB.begin(115200);
 
+  // CanSatKitRadio.begin returns true if communication with radio module
+  // onboard is correct
   bool ok = CanSatKitRadio.begin(CanSatPins_Radio_CS,
                                  CanSatPins_Radio_DIO0,
                                  433.0,
                                  Bandwidth_125000_Hz,
                                  SpreadingFactor_9,
                                  CodingRate_4_8);
+
   if (ok) {
+      // print that radio is ok
       SerialUSB.println("Radio OK!");
+      // stop printing radio debug to SerialUSB since everything is OK
       CanSatKitRadio.disable_debug();
   } else {
+      // print that radio configuration failed...
       SerialUSB.println("Radio failed!");
   }
 } 
 
 void loop() {
+
+  // CanSatKitRadio.transmit returns true if there is enough space in
+  // radio transmission buffer
   bool ok = CanSatKitRadio.transmit("Hello World!");
 
   if (!ok) {
+      // if CanSatKitRadio.transmit returned false -- print that buffer is full
+      // to prevent from buffer overflow - increase delay or send less data...
       SerialUSB.println("Transmit buffer full!");
   }
 
+  // adjust delay to prevent from radio transmission buffer overflow
   delay(10);
 }
--- examples/PressureSensor/PressureSensor.ino
+++ examples/PressureSensor/PressureSensor.ino
@@ -1,28 +1,54 @@
+// include CanSatKit library used for pressure sensor
 #include <CanSatKit.h>
 
+// BMP280 is a pressure sensor, create the sensor object
 BMP280 bmp;
 
 void setup() {
   SerialUSB.begin(9600);
+
+  // use begin() function to check if sensor is conneted and is able to communicate
+  // with the CanSat board
   if(!bmp.begin()) {
+    // if connection failed - print message to the user
     SerialUSB.println("BMP init failed!");
+    // the program will be 'halted' here and nothing will happen till restart
     while(1);
   } else {
+    // print message to the user if everything is OK
     SerialUSB.println("BMP init success!");
   }
   
+  // setOversampling function allows to set resolution of the pressure sensor
+  // possible values of setOversampling:
+  //  1 -- 2.62 Pa
+  //  2 -- 1.31 Pa
+  //  4 -- 0.66 Pa
+  //  8 -- 0.33 Pa
+  // 16 -- 0.16 Pa 
   bmp.setOversampling(16);
   
 }
 
 void loop() {
-  double T,P;
+  // declare variables for temperature (T) and pressure (P) readings
+  double T, P;
+
+  // start measurement - the function returns time that is neccessary to wait for result (ms)
+  // if communication with sensor fails the function returns 0, so the program is not interrupted
+  // (stopped) by broken device
   int result = bmp.startMeasurment();
 
+  // wait for proper amount of time according to startMeasurment function result
   delay(result);
+
+  // read temperature and pressure from BMP280 sensor and save values to T and P variables
   bmp.getTemperatureAndPressure(T,P);
-    
+  
+  // since we use it solely as pressure sensor print only "Pressure = " text
   SerialUSB.print("Pressure = ");
-  SerialUSB.print(P,2);
+  // print also value of pressure with two decimal places
+  SerialUSB.print(P, 2);
+  // print units - in this case the library returns hPa
   SerialUSB.println(" hPa");
 }
